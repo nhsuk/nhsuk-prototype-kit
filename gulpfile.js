43c98bdffe9e0869be7c6b6461b7b0ed24394418
AdamChrimes <iChrimes@live.co.uk>
2018-12-19 10:28:10 +0000

    Reorganise the folder structure into app/ and public/
    The folder structure of the project is now split into app/ and public/
    with app/ being the folder changes are made and public/ being where
    compiled files go. Also clean up some of the sass files imports to
    be cleaner.

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,13 @@ var rename = require("gulp-rename");
 var cleanCSS = require('gulp-clean-css');
 
 function styles() {
-  return gulp.src('app/styles/**/*.scss')
+  return gulp.src('app/assets/sass/**/*.scss')
     .pipe(sass())
     .pipe(cleanCSS())
     .pipe(rename({
       suffix: '.min'
     }))
-    .pipe(gulp.dest('app/assets/css'))
+    .pipe(gulp.dest('public/css'))
     .on('error', (err) => {
       console.log(err)
       process.exit(1)
@@ -18,7 +18,7 @@ function styles() {
 }
 
 function watch() {
-  gulp.watch('app/styles/**/*.scss', styles);
+  gulp.watch('app/assets/sass/**/*.scss', styles);
 }
 
 exports.watch = watch;
