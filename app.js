1b5fc8e5566553ea1f71977466971effc3b509a0
AdamChrimes <iChrimes@live.co.uk>
2019-01-04 09:27:36 +0000

    Configure basic env variables and password protection
    Look at using basic env variables to set the port and also
    to help set username/password for password protection of the prototype
    pages on production servers.

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
-const app = require('express')();
+// Required dependencies
 const express = require('express');
+const app = require('express')();
 const nunjucks = require('nunjucks');
 const path = require('path');
-const routing = require('./middleware/routing.js');
-const routes = require('./app/routes.js')
 
+// Custom middleware
+const autoRouting = require('./middleware/auto-routing');
+
+// Environment variables
+const PORT = process.env.PORT || 3000;
+const ENV = process.env.NODE_ENV || 'development';
+const USERNAME = process.env.USERNAME || 'hello';
+const PASSWORD = process.env.PASSWORD || 'goodbye';
+
+// Shared configuration
 app.set('view engine', 'html');
-app.set('port', process.env.PORT || 3000);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/nhsuk-frontend', express.static(path.join(__dirname, 'node_modules/nhsuk-frontend/packages')));
 
+// Nunjucks configuration
 var appViews = [
   path.join(__dirname, '/app/views/'),
   path.join(__dirname, '/docs/views/'),
@@ -23,14 +32,13 @@ nunjucks.configure(appViews, {
   watch: true
 })
 
-app.use('/', routes)
-
+// Automatic page routing
 app.get(/^([^.]+)$/, function (req, res, next) {
-  routing.matchRoutes(req, res, next)
+  autoRouting.matchRoutes(req, res, next)
 })
 
-app.listen(app.get('port'), function() {
-  console.log('App running at http://localhost:' + app.get('port'));
+app.listen(PORT, () => {
+  console.log(`App is running in ${ENV} mode at http://localhost:${PORT}`);
 });
 
 module.exports = app;
