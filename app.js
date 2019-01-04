5723dd550b7f266ffdaa0a4fed5b20ecabecad43
AdamChrimes <iChrimes@live.co.uk>
2019-01-04 13:43:14 +0000

    Handle the common port in use error with express
    If the selected port is being used by another process then this
    app will crash. Instead of crashing the app now attempts on run
    on a randomised port number.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path')
 // External dependencies
 const express = require('express');
 const nunjucks = require('nunjucks');
+const chalk = require('chalk');
 
 // Local dependencies
 const authentication = require('./middleware/authentication');
@@ -12,6 +13,7 @@ const config = require('./app/config');
 
 // Set configuration variables
 const PORT = config.port || process.env.PORT || 3000;
+const ENV = (process.env.NODE_ENV || 'development').toLowerCase()
 
 // Initialise applications
 const app = express()
@@ -40,8 +42,17 @@ app.use('/nhsuk-frontend', express.static(path.join(__dirname, 'node_modules/nhs
 // Application routes
 app.use('/', routes)
 
+// Run the application
 app.listen(PORT, () => {
-  console.log(`App is running at http://localhost:${PORT}`);
+  console.log(chalk.green(`App is running at http://localhost:${PORT}`));
+})
+.on( 'error', function (e) { 
+  if (ENV == 'development' && e.code == 'EADDRINUSE') { // If selected port is in use elsewhere
+    console.log(chalk.yellow(`Port ${PORT} is currently in use, switching to a random port...`));
+    randomPort = PORT + Math.floor(Math.random()*(999-100+1)+100); // Randomise the port number
+    app.listen(randomPort); // Set new random port
+    console.log(chalk.green(`App is running at http://localhost:${randomPort}`)); // Re-run the application on new port
+  }
 });
 
 module.exports = app;
