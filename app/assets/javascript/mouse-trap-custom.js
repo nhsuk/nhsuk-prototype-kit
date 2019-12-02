597a2bb3873fe0c72a741d2264d3fc511a41ba29
monsterlimo <mattnixon79@googlemal.com>
2019-12-02 13:16:24 +0000

    added research info
    design challenges and ntoes from research sessions

---
+++ app/assets/javascript/mouse-trap-custom.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+    Mousetrap.bind('?', function() { alert("Help Section - Keyboard Commands"); });
+
+  // search
+  Mousetrap.bind('ctrl e c', function(e) {
+    var frm = document.getElementById("searchfield") || null;
+    if (frm) {
+        document.getElementById("searchfield").focus();
+        document.getElementById('searchform').action = "http://localhost:3000/manage-participant/screening-history-a-notes";
+        return false;
+    } else {
+        window.location.href = "http://localhost:3000/manage-participant/screening-history-a-notes";
+    }
+  });
+
+  Mousetrap.bind('ctrl c y', function(e) {
+    var frm = document.getElementById("searchfield") || null;
+    if (frm) {
+        document.getElementById("searchfield").focus();
+        document.getElementById('searchform').action = "http://localhost:3000/manage-participant/screening-history-a#testresults";
+        return false;
+    } else {
+        window.location.href = "http://localhost:3000/manage-participant/screening-history-a#testresults";
+    }
+  });
+
+  Mousetrap.bind('up up down down left right left right b a', function() {
+    window.location.href = "https://www.konami.com/games/eu/en/products/contra_rc/";
+  });
+
+
+
+  
+
+  //document.getElementById("searchfield").onclick(console.log('clicked'));
+
+
+
+//  Mousetrap.bind('ctrl c y', function(e) {
+//    window.location.href = "http://localhost:3000/manage-participant/screening-history-a#test-results";
+//    return false;
+//  });
+
+  Mousetrap.bind('ctrl h p', function(e) {
+    window.location.href = "http://localhost:3000/manage-participant/screening-history-a-hpv";
+    return false;
+  });
+
+  Mousetrap.bind('ctrl e x i t', function() {
+      alert('Are you sure you want to leave?');
+  })
+  
+});
