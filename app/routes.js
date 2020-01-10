8d8663b49462a0b8d71743f8f7cebd2ae5dc8862
monsterlimo <mattnixon79@googlemal.com>
2020-01-10 15:01:28 +0000

    added version b of worklist

--- app/routes.js
+++ app/routes.js
@@ -5,3 +5,23 @@ const router = express.Router();
 // Add your routes here - above the module.exports line
 
 module.exports = router;
+
+
+// Branching example
+router.post('examples/branching/answer', function (req, res) {
+
+    console.log("testing testing search")
+    // Make a variable and give it the value from 'know-nhs-number'
+    //var nhsNumber = req.session.data['know-nhs-number']
+  
+    // Check whether the variable matches a condition
+    //if (nhsNumber == "Yes"){
+      // Send user to next page
+      //res.redirect('/docs/examples/branching/answer-yes')
+    //}
+    //else {
+      // Send user to ineligible page
+      //res.redirect('/docs/examples/branching/answer-no')
+   // }
+  
+  })
