1d3d51f863b1c4fdad5565b2c3eb635c3d0bb139
monsterlimo <mattnixon79@googlemal.com>
2020-01-22 09:37:18 +0000

    add more nhsuk components
    v5 version added and reduced the amount of custom components

--- app/routes.js
+++ app/routes.js
@@ -19,6 +19,58 @@ router.post('/csass/add-test-result/v1/change', function (req, res) {
     // Send user to ineligible page
     res.redirect('/csass/add-test-result/v1/cease')
   }
+})
+
+
+router.post('/v5/login/role-select', function(req, res) {
+  console.log('select a role');
+  var roleSelected = req.session.data['role'];
+  console.log('role: ' + roleSelected);
+  res.redirect('/v5/patient/search')
+})
+
+router.post('/search-v2/', function (req, res) {
+    
+  var nhsNumber = req.session.data['searchnhs']
+  // 3816158897 - invited
+  // 6170211547 - routine
+  // 7594384164 - colposcopy
+
+  if (nhsNumber == "3816158897") {
+      res.redirect('/sample-taker/v2/history')
+  }
+
+  if (nhsNumber == "6170211547") {
+      res.redirect('/sample-taker/v2/history-routine')
+  }
+  
+  if (nhsNumber == "7594384164") {
+      res.redirect('/sample-taker/v2/history-colposcopy')
+  }
+
+  console.log("not found")
+  //res.render("sample-taker/search/index.html")
+
+})
+
+
+router.post('/search-v5/', function (req, res) {
+    
+  var nhsNumber = req.session.data['searchnhs']
+
+  if (nhsNumber == "3816158897") {
+      res.redirect('/v5/patient/screening-history')
+  }
+
+  if (nhsNumber == "6170211547") {
+      res.redirect('/v5/patient/history-routine')
+  }
+  
+  if (nhsNumber == "7594384164") {
+      res.redirect('/v5/patient/history-colposcopy')
+  }
+
+  res.redirect('/v5/patient/screening-history')
 
 })
 
@@ -26,26 +78,3 @@ module.exports = router;
 
 
 // Branching example
-router.post('/search-v2/', function (req, res) {
-    
-    var nhsNumber = req.session.data['searchnhs']
-    // 3816158897 - invited
-    // 6170211547 - routine
-    // 7594384164 - colposcopy
-
-    if (nhsNumber == "3816158897") {
-        res.redirect('/sample-taker/v2/history')
-    }
-
-    if (nhsNumber == "6170211547") {
-        res.redirect('/sample-taker/v2/history-routine')
-    }
-    
-    if (nhsNumber == "7594384164") {
-        res.redirect('/sample-taker/v2/history-colposcopy')
-    }
-
-    console.log("not found")
-    //res.render("sample-taker/search/index.html")
-  
-  })
