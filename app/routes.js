848328ccbfbddc4cb58918c113fdf3eeb4205bb5
monsterlimo <mattnixon79@googlemal.com>
2020-03-03 14:12:03 +0000

    v7 of the prototype

--- app/routes.js
+++ app/routes.js
@@ -49,6 +49,28 @@ router.post("/v6/patient/change-due-date/change", function(req, res) {
   }
 });
 
+router.post("/v7/patient/change-due-date/change", function(req, res) {
+  // Make a variable and give it the value from 'know-nhs-number'
+  var reason = req.session.data["reason"];
+  // Check whether the variable matches a condition
+
+  if (
+    reason == "No cervix" ||
+    reason == "Patient choice" ||
+    reason == "Mental capacity act" ||
+    reason == "Receiving radiotherapy" ||
+    reason == "Aged over 65" ||
+    reason == "Patient choice" ||
+    reason == "Other reason"
+  ) {
+    // Send user to next page
+    res.redirect("/v7/patient/change-due-date/enter-reason-check-cease");
+  } else {
+    // Send user to ineligible page
+    res.redirect("/v7/patient/change-due-date/enter-reason-check-defer");
+  }
+});
+
 // CEASE / DEFER
 router.post("/csass/add-test-result/v2/change", function(req, res) {
   // Make a variable and give it the value from 'know-nhs-number'
@@ -78,16 +100,6 @@ router.post("/csass/add-test-result/v1/change", function(req, res) {
   }
 });
 
-router.post("/v5/login/role-select", function(req, res) {
-  var roleSelected = req.session.data["role"];
-  res.redirect("/v5/patient/search/search");
-});
-
-router.post("/v6/login/role-select", function(req, res) {
-  var roleSelected = req.session.data["role"];
-  res.redirect("/v6/patient/search/search");
-});
-
 // NEXT
 router.post("/v5/change-due-date", function(req, res) {
   if (req.session.data["recall"] == "defer") {
@@ -100,6 +112,7 @@ router.post("/v5/change-due-date", function(req, res) {
 
   res.redirect("/v5/patient/patient-summary-deferred");
 });
+
 //NEXT
 router.post("/v6/change-due-date", function(req, res) {
   if (req.session.data["recall"] == "defer") {
@@ -111,6 +124,18 @@ router.post("/v6/change-due-date", function(req, res) {
   }
 
   res.redirect("/v6/patient/patient-summary-deferred");
+});
+
+router.post("/v7/change-due-date", function(req, res) {
+  if (req.session.data["recall"] == "defer") {
+    res.redirect("/v7/patient/patient-summary-deferred");
+  }
+
+  if (req.session.data["recall"] == "cease") {
+    res.redirect("/v7/patient/patient-summary-ceased");
+  }
+
+  res.redirect("/v7/patient/patient-summary-deferred");
 });
 
 //NEXT
@@ -219,6 +244,57 @@ router.post("/search-v6/", function(req, res) {
   }
 
   res.redirect("/v6/patient/patient-summary");
+});
+
+router.post("/search-v7/", function(req, res) {
+  var nhsNumber = req.session.data["searchnhs"];
+
+  // Routine recall
+  // Recall is 5 years away
+  if (nhsNumber == "6170211547") {
+    req.session.data["nhsNumber"] = "617 021 1547";
+    req.session.data["title"] = "Miss";
+    req.session.data["firstName"] = "Josie";
+    req.session.data["lastName"] = "Jackson";
+    req.session.data["dob"] = "29 years (4 June 1991)";
+    req.session.data["ntdd"] = "09 04 2025";
+    req.session.data["reason"] = "";
+    req.session.data["status"] = "ROUTINE";
+    req.session.data["address"] = "19 Polly Fall Close, Bradford, BD10 3RT";
+    res.redirect("/v7/patient/patient-summary");
+  }
+
+  // Referred to colposcopy
+  // Recall is 6 months away
+  if (nhsNumber == "7594384164") {
+    req.session.data["nhsNumber"] = "759 438 4164";
+    req.session.data["title"] = "Mrs";
+    req.session.data["firstName"] = "Francesca";
+    req.session.data["lastName"] = "Williams";
+    req.session.data["dob"] = "40 years (15 Dec 1979)";
+    req.session.data["ntdd"] = "09 04 2020";
+    req.session.data["reason"] = "";
+    req.session.data["status"] = "REFERRED TO COLPOSCOPY";
+    req.session.data["address"] = "19 Polly Fall Close, Bradford, BD10 3RT";
+    res.redirect("/v7/patient/patient-summary");
+  }
+
+  // CEASED
+  if (nhsNumber == "3816158897") {
+    req.session.data["nhsNumber"] = "381 615 8897";
+    req.session.data["title"] = "Mrs";
+    req.session.data["firstName"] = "Francesca";
+    req.session.data["lastName"] = "Williams";
+    req.session.data["dob"] = "40 years (15 Dec 1979)";
+    req.session.data["ntdd"] = "09 04 2020";
+    req.session.data["status"] = "ceased";
+    req.session.data["reason"] = "no cervix";
+    req.session.data["address"] = "19 Polly Fall Close, Bradford, BD10 3RT";
+    req.session.data["alreadyCeased"] = true;
+    res.redirect("/v7/patient/patient-summary");
+  }
+
+  res.redirect("/v7/patient/patient-summary");
 });
 
 module.exports = router;
