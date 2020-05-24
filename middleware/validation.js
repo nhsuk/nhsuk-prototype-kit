3d2224556138f47909b3f7b3b0633ffcfd5cf69b
Tom Doughty <thomashdoughty@gmail.com>
2020-05-24 12:18:38 +0100

    Add to how toexamples

--- middleware/validation.js
+++ middleware/validation.js
@@ -1,38 +1,46 @@
 // Use express validator depencency
 const { validationResult } = require('express-validator');
 const validationRules = require('../app/validation-rules');
+const docsValidationRules = require('../docs/documentation_validation_rules');
 const { matchRoutesRefererPath } = require('./auto-routing');
 
-module.exports = [validationRules, (req, res, next) => {
+const rules = [...validationRules, ...docsValidationRules];
+
+module.exports = [rules, (req, res, next) => {
   // Reset any previous errors
-  req.session.errorsList = [];
-  req.session.errorsObj = {};
+  req.session.errors = {};
   // Only run validation on POST requests
   if (req.method === 'POST') {
-    // Get errors from validationRules middleware
-    const reqErrors = validationResult(req);
-    // If there are any errors
-    if (!reqErrors.isEmpty()) {
-      // Set errors to session for use in Nunjucks templates
-      req.session.errors = reqErrors.array().reduce((obj, { param, msg }) => ({
-        // Array of objects with href and text properties
-        errorsList: [...obj.errorsList, {
-          href: `#${param}`,
-          text: msg
-        }],
-        // Object of errors with { param , msg }
-        errorsObj: {
-          ...obj.errorsObj,
-          [param]: msg
+    // Get array of all keys in the request body 
+    const errorsToValidate = Object.keys(req.body);
+    // Set errors to session for use in Nunjucks templates
+    const nhsErrors = validationResult(req).array().reduce((obj, { param, msg }) => {
+      // Only return errors for inputs on this page 
+      if (errorsToValidate.includes(param)) {
+        return {
+          // Array of objects with href and text properties
+          errorsList: [...obj.errorsList, {
+            href: `#${param}`,
+            text: msg
+          }],
+          // Object of errors with { param , msg }
+          errorsObj: {
+            ...obj.errorsObj,
+            [param]: msg
+          }
         }
-      }), {
-        // Set default empty arrays and objects
-        errorsList: [],
-        errorsObj: {}
-      });
+      }
+      return obj;
+    }, {
+      // Set default empty arrays and objects
+      errorsList: [],
+      errorsObj: {}
+    });
+    if (nhsErrors.errorsList.length) {
+      req.session.errors = nhsErrors;
       // Redirect back to referer route
-      return matchRoutesRefererPath(req, res, next)
-    };
+      return matchRoutesRefererPath(req, res, next);
+    }
   }
   return next();
 }];
