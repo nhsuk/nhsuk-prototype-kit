08032db24d87fa601aea7c0ac268e65d900a9282
David Evans <davidevans@Davids-MacBook-Pro-3.local>
2020-07-16 11:03:45 +0100

    Fixed comments

--- app/assets/javascript/comments.js
+++ app/assets/javascript/comments.js
@@ -1,14 +1,8 @@
 $(document).ready(function() {
-
-
 	$('button').click(function() {
-
-
-		var comment = $('.commentBox').val(); // This is the text area
-
+		var comment = $('.commentBox').val();
 		$('<li class="nhsuk-panel">').text(comment).prependTo('.nhsuk-comment-container');
-
-		//$('button').attr('enabled');
+		$('button').attr('disabled', 'true');
 		$('.counter').text('140');
 		$('.commentBox').val('');
 	});
@@ -18,5 +12,17 @@ $(document).ready(function() {
 		var charLeft =  140 - commentLength;
 		$('.counter').text(charLeft);
 
+		if (commentLength == 0) {
+			$('button').attr('disabled', 'true');
+		}
+		else if (commentLength > 140) {
+			$('button').attr('disabled', 'true');
+		}
+		else {
+			$('button').removeAttr('disabled', 'true');
+		}
+	});
+
+	$('button').attr('disabled', 'true');
 
 });
--- app/assets/javascript/date.js
+++ app/assets/javascript/date.js
@@ -1,4 +1,20 @@
-var today = new Date();
-var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-var dateTime = date+' '+time;
+// Filter to print out dates in NHS App format
+  //
+  // Accepts numbers and strings.
+  // Strings for "today" and "tomorrow"
+  // Numbers like 5 will return a day 5 days away
+  //
+  // Usage in your templates:
+  //
+  // {{ "today" | returnDate }}
+  // {{ "tomorrow" | returnDate }}
+  // {{ 5 | returnDate }}
+  filters.returnDate = function(date) {
+    if (date == "today") {
+      return moment().format("dddd D MMMM YYYY");
+    } else if (date == "tomorrow") {
+      return moment().add(1, 'days').format("dddd D MMMM YYYY");
+    } else {
+      return moment().add(date, 'days').format("dddd D MMMM YYYY");
+    }
+  }
