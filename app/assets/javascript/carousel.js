4b34ddc9cc7290141f3a91b60f6827e936d39224
David Evans <davidevans@Davids-MacBook-Pro-3.local>
2020-11-19 12:43:52 +0000

    Added carousel

---
+++ app/assets/javascript/carousel.js
@@ -0,0 +1,44 @@
+var carousel = $('#carousel'),
+    threshold = 150,
+    slideWidth = 500,
+    dragStart,
+    dragEnd;
+
+$('#next').click(function(){ shiftSlide(-1) })
+$('#prev').click(function(){ shiftSlide(1) })
+
+carousel.on('mousedown', function(){
+  if (carousel.hasClass('transition')) return;
+  dragStart = event.pageX;
+  $(this).on('mousemove', function(){
+    dragEnd = event.pageX;
+    $(this).css('transform','translateX('+ dragPos() +'px)')
+  })
+  $(document).on('mouseup', function(){
+    if (dragPos() > threshold) { return shiftSlide(1) }
+    if (dragPos() < -threshold) { return shiftSlide(-1) }
+    shiftSlide(0);
+  })
+});
+
+function dragPos() {
+  return dragEnd - dragStart;
+}
+
+function shiftSlide(direction) {
+  if (carousel.hasClass('transition')) return;
+  dragEnd = dragStart;
+  $(document).off('mouseup')
+  carousel.off('mousemove')
+          .addClass('transition')
+          .css('transform','translateX(' + (direction * slideWidth) + 'px)');
+  setTimeout(function(){
+    if (direction === 1) {
+      $('.slide:first').before($('.slide:last'));
+    } else if (direction === -1) {
+      $('.slide:last').after($('.slide:first'));
+    }
+    carousel.removeClass('transition')
+		carousel.css('transform','translateX(0px)');
+  },700)
+}
