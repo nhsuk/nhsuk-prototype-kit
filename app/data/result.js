509e3f52b3cdafeff6cc178cd3118c174a215d6d
Zoe <zoe.mcclatchey1@nhs.net>
2021-07-21 16:50:08 +0100

    dummy changes back

--- app/data/result.js
+++ app/data/result.js
@@ -6,6 +6,9 @@ var moment = require('moment');
 // S - Suspended
 
 const results = [
+    
+   
+    
     {
         "result_ID": Math.random().toString(16).slice(2),
         "action": "Routine",
@@ -27,27 +30,7 @@ const results = [
         "source_code": "G",
         "test_date": moment().subtract(146, "weeks")
     },
-    {
-        "result_ID": Math.random().toString(16).slice(2),
-        "action": "Routine",
-        "action_code": "A",
-        "created": "2020-05-28T16:35:45.890",
-        "is_deleted": false,
-        "letter_status": "Sent",
-        "nhs_number": "9100001384",
-        "participant_id": "8d0243e8-2ef0-4d4d-be8c-06be86138bfc",
-        "recall_months": "60",
-        "result": "Negative",
-        "result_code": "2",
-        "result_date": moment().subtract(146, "weeks"),
-        "result-type": "English",
-        "sender_code": "861321",
-        "sending_lab": "60210",
-        "slide_number": "89007139",
-        "sort_key": "RESULT#1989-05-23#2020-05-28T16:35:45.890",
-        "source_code": "G",
-        "test_date": moment().subtract(146, "weeks")
-    },
+    
     {
         "result_ID": Math.random().toString(16).slice(2),
         "action": "Routine",
