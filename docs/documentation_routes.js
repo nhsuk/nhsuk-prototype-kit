d4620fcc3e5208d55b708184b71d8dcc1e0f0068
Frankie Roberto <frankie@frankieroberto.com>
2024-11-05 22:18:49 +0000

    Remove docs index page and others

--- docs/documentation_routes.js
+++ docs/documentation_routes.js
@@ -4,24 +4,4 @@ const packageJson = require('../package.json');
 
 const router = express.Router();
 
-// Documentation router
-router.get('/', (req, res) => {
-  res.render('index');
-});
-
-// Branching example
-router.post('/examples/branching/answer', (req, res) => {
-  // Make a variable and give it the value from 'know-nhs-number'
-  const nhsNumber = req.session.data['know-nhs-number'];
-
-  // Check whether the variable matches a condition
-  if (nhsNumber === 'Yes') {
-    // Send user to next page
-    res.redirect('/docs/examples/branching/answer-yes');
-  } else {
-    // Send user to ineligible page
-    res.redirect('/docs/examples/branching/answer-no');
-  }
-});
-
 module.exports = router;
