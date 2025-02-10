daedfa00be70b665c4196caec0ada3763162f0f4
Frankie Roberto <frankie@frankieroberto.com>
2025-02-10 14:41:26 +0000

    Add Content-Security-Policy

--- app.js
+++ app.js
@@ -194,6 +194,10 @@ if (process.env.NODE_ENV === 'production') {
     // Set Strict-Transport-Security header to
     // ensure that browsers only use HTTPS
     res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
+
+    // Set content security policy to upgrade
+    // all HTTP requests to HTTPS
+    res.setHeader('Content-Security-Policy', 'upgrade-insecure-requests');
     next();
   });
 }
