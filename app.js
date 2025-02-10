ddace4028ecefd7c828b549a6e1b4469091ed658
Frankie Roberto <frankie@frankieroberto.com>
2025-02-10 15:22:33 +0000

    Refactor to move production headers into lib/

--- app.js
+++ app.js
@@ -78,20 +78,9 @@ const sessionOptions = {
 };
 
 if (process.env.NODE_ENV === 'production') {
-  app.use((req, res, next) => {
-    // Set Strict-Transport-Security header to
-    // ensure that browsers only use HTTPS
-    res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
-
-    // Set content security policy to upgrade
-    // all HTTP requests to HTTPS
-    res.setHeader('Content-Security-Policy', 'upgrade-insecure-requests');
-    next();
-  });
+  app.use(production);
+  app.use(authentication);
 }
-
-// Authentication
-app.use(authentication);
 
 // Support session data in cookie or memory
 if (useCookieSessionStore === 'true') {
