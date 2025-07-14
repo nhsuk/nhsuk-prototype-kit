54aa460b02fb25a8aff710ab421352d3f781d3c8
Colin Rotherham <work@colinr.com>
2025-07-14 16:11:41 +0100

    Add default port

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const prototypeAdminRoutes = require('./lib/middleware/prototype-admin-routes');
 const exampleTemplatesRoutes = require('./lib/example_templates_routes');
 
 // Set configuration variables
-const port = parseInt(process.env.PORT || config.port, 10);
+const port = parseInt(process.env.PORT || config.port, 10) || 2000;
 
 // Initialise applications
 const app = express();
