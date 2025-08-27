67f3b5f3a28c0c04a27c054ab740b967d20797e7
Colin Rotherham <work@colinr.com>
2025-08-27 15:09:20 +0100

    Switch to StandardJS for javascript linting

--- linters/.eslintrc.js
+++ linters/.eslintrc.js
@@ -1,5 +1,26 @@
 module.exports = {
-    "extends": [
-        "airbnb-base"
-    ],
+  overrides: [
+    {
+      files: ['**/*.js'],
+      extends: [
+        'eslint:recommended',
+        'plugin:import/recommended',
+        'plugin:@typescript-eslint/strict',
+        'plugin:@typescript-eslint/stylistic'
+      ],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        ecmaVersion: 'latest'
+      },
+      plugins: [
+        '@typescript-eslint',
+        'import',
+        'jsdoc',
+        'n',
+        'promise',
+        'jest',
+        'jest-dom'
+      ]
+    }
+  ]
 };
