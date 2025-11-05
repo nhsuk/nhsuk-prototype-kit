c7aee4c46dcd48b0c5b4638690ca5cbe3bde95fb
Frankie Roberto <frankie@frankieroberto.com>
2025-11-05 22:15:52 +0000

    Remove app settings

--- app.js
+++ app.js
@@ -33,7 +33,6 @@ const port = parseInt(process.env.PORT || config.port, 10) || 2000
 
 // Initialise applications
 const app = express()
-const exampleTemplatesApp = express()
 
 // Add variables that are available in all views
 app.locals.asset_path = '/public/'
@@ -118,20 +117,6 @@ if (!sessionDataDefaultsFileExists) {
 
 // Local variables
 app.use(locals(config))
-
-// View engine
-app.set('view engine', 'html')
-exampleTemplatesApp.set('view engine', 'html')
-
-// Support for parsing nested query strings
-// https://github.com/nhsuk/nhsuk-prototype-kit/issues/644
-app.set('query parser', 'extended')
-
-// This setting trusts the X-Forwarded headers set by
-// a proxy and uses them to set the standard header in
-// req. This is needed for hosts like Heroku.
-// See https://expressjs.com/en/guide/behind-proxies.html
-app.set('trust proxy', 1)
 
 // Use public folder for static assets
 app.use(express.static(join(__dirname, 'public')))
