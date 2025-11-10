9564c4d1cc934ac0dcdf63e8769e0be2ae8eb4b1
Colin Rotherham <work@colinr.com>
2025-11-10 14:56:37 +0000

    Skip ES module transforms for browsers

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,37 @@
-module.exports = {
-  browserslistEnv: 'javascripts',
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        // Apply bug fixes to avoid transforms
-        bugfixes: true,
+/**
+ * Babel config
+ *
+ * @type {ConfigFunction}
+ */
+module.exports = function (api) {
+  // Assume browser environment via Gulp plugin
+  const isBrowser = api.caller((caller) => caller?.name === 'babel-gulp')
 
-        // Apply smaller "loose" transforms for browsers
-        loose: true
+  return {
+    browserslistEnv: 'javascripts',
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          // Apply bug fixes to avoid transforms
+          bugfixes: true,
+
+          // Apply smaller "loose" transforms for browsers
+          loose: isBrowser,
+
+          // Skip ES module transforms for browsers
+          modules: isBrowser ? false : 'auto'
+        }
+      ]
+    ],
+    env: {
+      test: {
+        browserslistEnv: 'node'
       }
-    ]
-  ],
-  env: {
-    test: {
-      browserslistEnv: 'node'
     }
   }
 }
+
+/**
+ * @import { ConfigFunction } from '@babel/core';
+ */
